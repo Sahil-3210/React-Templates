23a50fd37654102504c2d78654499b28d1976eea
Sahil Randive <randivesahil7@gmail.com>
2024-04-25 00:05:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+import Navigation from './conponent/Navigation'
+import Hero from './conponent/Hero'
+
+const App = () => {
+  return (
+    <div>
+      <Navigation></Navigation>
+      <Hero></Hero>
+    </div>
+  )
+}
+
+export default App
---
+++ src/conponent/Hero.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <main className='hero container'>
+        <div className='hero-content'>
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVER THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES. YOUR FEET DESERVER THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.</p>
+
+            <div className='hero-btn'>
+                <button>Shop Now</button>
+                <button className='category-btn'>Category</button>
+            </div>
+
+            <div className='shopping'>
+                <p>
+                    Also available on
+                </p>
+
+                <div className='brand-icons'>
+                    <img src='/images/amazon.png' alt='amazon-logo'></img>
+                    <img src='/images/flipkart.png' alt='flipkart-logo'></img>
+                </div>
+            </div>
+
+        </div>
+        <div className='hero-image'>
+        <img src='/images/shoe_image.png' alt='shoe_image-logo'></img>
+        </div>
+    </main>
+  )
+}
+
+export default Hero
